afcbba251d148b7faeef75753e6cdc4663b9b4b3
Manasa <manasa.7m@gmail.com>
2025-07-31 01:12:41 -0400

    updated admin routes to get all projects

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -2,10 +2,23 @@ import express from "express"
 import { createProject, allProjects, projectById, updateProject, deleteProject } from "../controllers/projectController.js";
 import { authMiddleware } from "../utils/auth.js";
 import adminOnly from "../utils/adminOnly.js";
+import Project from "../models/Project.js";
+import User from "../models/User.js";
 
 const router = express.Router();
 
 router.use(authMiddleware);
+
+
+//Admin only route 
+
+router.get('/adminprojects', authMiddleware, adminOnly, async  (req,res) => {
+    const role = req.user.role
+    console.log(role)
+    const projects = await Project.find().select({_id:1, name: 1}).populate("user", "username");
+    res.json(projects)
+})
+
 
 router.post('/', createProject)
 // router.post('/', adminOnly, projectCreate) // check before implementing
@@ -13,6 +26,8 @@ router.get('/', allProjects)
 router.get('/:id', projectById)
 router.put('/:id', updateProject)
 router.delete('/:id', deleteProject)
+
+
 
 // router.delete('/:id',adminOnly, deleteProject) // check before implementing
 
